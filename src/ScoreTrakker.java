c89361001554bd4995ff0077406855148d9e2fc8
Matthew-Yamamoto <matthew_yamamoto@mines.edu>
2025-10-02 12:02:32 -0600

    basic method outlines created

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
 public class ScoreTrakker {
 
+	private ArrayList<Student> students = null;
+	
+	public void loadDataFile(String s) {
+		
+	}
+	
+	public void printInOrder() {
+		
+	}
+	
+	public void processFiles() {
+		loadDataFile("PLACEHOLDER");
+		printInOrder();
+	}
+	
+	
+	public static void main(String[] args) {
+		ScoreTrakker scoreTrakker = new ScoreTrakker();
+		scoreTrakker.processFiles();
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,24 @@
+import java.util.*;
 
-public class Student {
+public class Student implements Comparable<Student>{
 
+	private String name = null;
+	private int score = 0;
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	@Override
+	public int compareTo(Student o) {
+		if(score > o.score) {
+			return 1;
+		}
+		return -1;
+	}
+	
+	@Override
+	public String toString() {
+		return (name + " " + score);
+	}
 }
