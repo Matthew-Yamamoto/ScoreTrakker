efb3f333566b8de9fac81fd86d5b0ccad8f83d94
Matthew-Yamamoto <matthew_yamamoto@mines.edu>
2025-10-02 15:56:20 -0600

    finished basic io stuff

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,19 +1,39 @@
 import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+
 
 public class ScoreTrakker {
 
-	private ArrayList<Student> students = null;
+	private ArrayList<Student> students = new ArrayList<Student>();
 	
-	public void loadDataFile(String s) {
-		
+	public void loadDataFile(String file) throws FileNotFoundException {
+		FileReader reader = new FileReader(file);
+		Scanner in = new Scanner(reader);
+		while(in.hasNextLine()) {
+			String name = in.nextLine();
+			String score = in.nextLine();
+			
+			//students.add(new Student(name,score));
+		}
+
 	}
 	
 	public void printInOrder() {
-		
+		for(Student student : students) {
+			student.compareTo(student);
+		}
 	}
 	
 	public void processFiles() {
-		loadDataFile("PLACEHOLDER");
+		String file = "scores.txt";
+		try {
+			loadDataFile(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("Cant open file: " + file);
+		}
 		printInOrder();
 	}
 	
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 
 public class Student implements Comparable<Student>{
 
