f04eb38dc972797d091ceeaa320fd7845b59d3c3
mandalili2005 <li2@mines.edu>
2025-10-03 12:48:28 -0600

    updated scoretrakker

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,56 @@
-
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 public class ScoreTrakker {
-
+	private ArrayList<Student> students = new ArrayList<Student>();
+	
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
+	
+	public void loadDataFile(String file) throws FileNotFoundException {
+		FileReader reader = new FileReader(file);
+		Scanner in = new Scanner(reader);
+		while(in.hasNextLine()) {
+			String name = in.nextLine();
+			String score = in.nextLine();
+			
+			
+			try {
+				int intScore = Integer.parseInt(score);
+				
+				students.add(new Student(name,intScore));
+				
+			} catch (NumberFormatException e) {
+				System.out.println("Incorrect format for " + name + " not a valid score: " + score);
+				
+			}
+			
+		}
+		
+	}
+	
+	public void printInOrder() {
+		for(Student student : students) {
+			System.out.println(student);
+			student.compareTo(student);
+		}
+	}
+	
+	public void processFiles() {
+		for (int i = 0; i < files.length; i++) {
+			String file = files[i];
+			try {
+				loadDataFile(file);
+			} catch (FileNotFoundException e) {
+				System.out.println("Cant open file: " + file);
+			}
+			printInOrder();
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		ScoreTrakker scoreTrakker = new ScoreTrakker();
+		scoreTrakker.processFiles();
+	}
 }
---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student implements Comparable<Student>{
+	private String name = null;
+	private int score = 0;
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	@Override
+	public int compareTo(Student o) {
+		if(score > o.score) {
+			return 1;
+		}
+		return -1;
+	}
+	
+	@Override
+	public String toString() {
+		return (name + " " + score);
+	}
+}
+
