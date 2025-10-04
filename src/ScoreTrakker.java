7abd08c1894c486689247beee80f87ed6ac3eb65
Matthew-Yamamoto <matthew_yamamoto@mines.edu>
2025-10-03 23:14:22 -0600

    Final commits

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,18 +1,22 @@
+//This class is the scoretrakker class where we load data files and print all of students and their scores
+
+
 import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 public class ScoreTrakker {
 	private ArrayList<Student> students = new ArrayList<Student>();
 	
-	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
+	private String[] files = {"scores.txt", "badscores.txt", "nofile.txt" };
 	
+	//Loads the given file and reads throws an error if the file doesnt exist
+	//adds all the students in the file to an arrayList
 	public void loadDataFile(String file) throws FileNotFoundException {
 		FileReader reader = new FileReader(file);
 		Scanner in = new Scanner(reader);
 		while(in.hasNextLine()) {
 			String name = in.nextLine();
 			String score = in.nextLine();
-			
 			
 			try {
 				int intScore = Integer.parseInt(score);
@@ -25,30 +29,35 @@ public class ScoreTrakker {
 			}
 			
 		}
-		
+		in.close();
 	}
 	
+	// prints all the students in order based on their score
 	public void printInOrder() {
+		System.out.println("Student Score List");
+		Collections.sort(students);
 		for(Student student : students) {
 			System.out.println(student);
-			student.compareTo(student);
 		}
+		System.out.println();
 	}
 	
+	//takes the array of files and calls loadDataFile on each file
 	public void processFiles() {
 		for (int i = 0; i < files.length; i++) {
 			String file = files[i];
 			try {
 				loadDataFile(file);
+				printInOrder();
 			} catch (FileNotFoundException e) {
 				System.out.println("Cant open file: " + file);
 			}
-			printInOrder();
+			students.clear();
 		}
 		
 	}
 	
-	
+	//main function to make a scoreTrakker object and call processfiles on it.
 	public static void main(String[] args) {
 		ScoreTrakker scoreTrakker = new ScoreTrakker();
 		scoreTrakker.processFiles();
--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,25 @@
+//This clsss is a Student object that has a name and a score 
+
 public class Student implements Comparable<Student>{
 	private String name = null;
 	private int score = 0;
+	
+	//This is the constuctor to initialize the name and score for this student
 	public Student(String name, int score) {
 		this.name = name;
 		this.score = score;
 	}
 	
+	//compares the score and returns 1 if its higher and -1 if lower
 	@Override
 	public int compareTo(Student o) {
-		if(score > o.score) {
+		if(score < o.score) {
 			return 1;
 		}
 		return -1;
 	}
 	
+	//name score format for printing the student
 	@Override
 	public String toString() {
 		return (name + " " + score);
